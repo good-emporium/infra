36976c63ed53bd2d11d88276f7e27f2152961f29
Dashiel Lopez Mendez <lopez.mendez.d@gmail.com>
2018-06-22 11:07:59 -0500

    Switching from s3 acl to bucket policy

--- modules/web/storage/main.tf
+++ modules/web/storage/main.tf
@@ -1,6 +1,18 @@
 resource "aws_s3_bucket" "web" {
   bucket = "${local.web_bucket_prefix}-${var.env}"
-  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+    "Sid":"PublicReadForGetBucketObjects",
+    "Effect":"Allow",
+    "Principal": "*",
+    "Action":["s3:GetObject"],
+    "Resource":["arn:aws:s3:::${local.web_bucket_prefix}-${var.env}/*"]
+  }]
+}
+EOF
 
   logging {
     target_bucket = "${var.logging_bucket}"
