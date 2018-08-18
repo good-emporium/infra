ad5ec82ea93dc8d817da363ec0be45eef11267ed
Jen Udan <jen.udan@gmail.com>
2018-08-17 20:38:46 -0500

    Created user tables (#2)

--- scripts/api/dynamo.tf
+++ scripts/api/dynamo.tf
@@ -15,3 +15,21 @@ module "api_orgs_prod" {
   hash_key = "id"
   name     = "organizations"
 }
+
+module "api_users_dev" {
+  source = "../../modules/api/dynamo"
+
+  capacity = 1
+  env      = "d"
+  hash_key = "id"
+  name     = "users"
+}
+
+module "api_users_prod" {
+  source = "../../modules/api/dynamo"
+
+  capacity = 20
+  env      = "p"
+  hash_key = "id"
+  name     = "users"
+}
