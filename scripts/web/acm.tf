a9a3273c1fdf1506dcbe5a2a19dd93eeb4a587d6
Dashiel Lopez Mendez <lopez.mendez.d@gmail.com>
2018-09-12 22:12:15 -0500

    Added acm cert validation

--- scripts/web/acm.tf
+++ scripts/web/acm.tf
@@ -7,3 +7,16 @@ resource "aws_acm_certificate" "goodemporium_com" {
 
   subject_alternative_names = ["${local.com_domain_root}"]
 }
+
+resource "aws_route53_record" "cert_validation" {
+  name = "${aws_acm_certificate.goodemporium_com.domain_validation_options.0.resource_record_name}"
+  type = "${aws_acm_certificate.goodemporium_com.domain_validation_options.0.resource_record_type}"
+  zone_id = "${module.init.ge_zone_id}"
+  records = ["${aws_acm_certificate.goodemporium_com.domain_validation_options.0.resource_record_value}"]
+  ttl = 60
+}
+
+//resource "aws_acm_certificate_validation" "cert" {
+//  certificate_arn = "${aws_acm_certificate.goodemporium_com.arn}"
+//  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+//}
